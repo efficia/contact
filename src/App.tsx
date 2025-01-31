58da94e25fb1274c8942abe0d0c190f556e90081
Dilith Achalan <dilith.achalan@outlook.com>
2025-01-31 22:31:30 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <Router basename="/contact">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contact/:id" element={<Contact />} />
+        <Route path="/:id" element={<Contact />} />
       </Routes>
     </Router>
   );
